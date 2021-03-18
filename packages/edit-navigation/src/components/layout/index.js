6985e1bfd11b2a96bbe96562ebbad1661f91a09b
Daniel Richards <daniel.richards@automattic.com>
2021-03-18 11:00:34 +0800

    Implement some basic styles

--- packages/edit-navigation/src/components/layout/index.js
+++ packages/edit-navigation/src/components/layout/index.js
@@ -127,7 +127,7 @@ export default function Layout( { blockEditorSettings } ) {
 										<Popover.Slot name="block-toolbar" />
 
 										{ ! isLargeViewport && (
-											<div className="edit-widgets-header__block-toolbar">
+											<div className="edit-navigation-layout__block-toolbar">
 												<BlockToolbar hideDragHandle />
 											</div>
 										) }
